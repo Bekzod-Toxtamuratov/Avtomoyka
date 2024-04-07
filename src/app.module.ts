e1b5f5e261e946baeb94524e8402a6b554a33efe
BekzodToxtamuratov <bekzodtoxtamuratov@gmail.com>
2024-04-07 23:20:10 +0500

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-
 import { BotModule } from './modules/bot/bot.module';
-import { ClientModule } from './modules/client/client.module';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { BotName } from './app.constants';
+import { Bot } from './modules/bot/models/bot.models';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 @Module({
   imports: [
     TelegrafModule.forRootAsync({
@@ -11,11 +12,23 @@ import { BotName } from './app.constants';
       useFactory: () => ({
         token: '6313671284:AAE0VuuQB5vee2ZHV4Ubu83Gc9fddVYQw44',
         middlewares: [],
-        include: [BotModule],
+        include: [BotModule, Bot],
       }),
     }),
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [Bot],
+      autoLoadModels: true,
+      sync: { alter: true },
+      logging: false,
+    }),
     BotModule,
-    ClientModule,
   ],
 })
 export class AppModule {}
--- src/modules/bot/bot.service.ts
+++ src/modules/bot/bot.service.ts
@@ -1,25 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { InjectBot } from 'nestjs-telegraf';
+import { Context, Telegraf } from 'telegraf';
+import { InjectModel } from '@nestjs/sequelize';
+import { BotName } from '../../app.constants';
+import { Bot } from './models/bot.models';
+import { Car } from './models/car';
+
 @Injectable()
-
 export class BotService {
+  constructor(
+    @InjectModel(Bot) private botRepo: typeof Bot,
+    @InjectModel(Car) private carRepo: typeof Car,
+    @InjectBot(BotName) private readonly bot: Telegraf<Context>,
+  ) {}
 
-  // create(createBotDto: CreateBotDto) {
-  //   return 'This action adds a new bot';
-  // }
-  // findAll() {
-  //   return `This action returns all bot`;
-  // }
-  // findOne(id: number) {
-  //   return `This action returns a #${id} bot`;
-  // }
-  // update(id: number, updateBotDto: UpdateBotDto) {
-  //   return `This action updates a #${id} bot`;
-  // }
-  // remove(id: number) {
-  //   return `This action removes a #${id} bot`;
-  // }
+  async start(ctx: Context) {
+    try {
+      const userId = ctx.from.id;
+      const user = await this.botRepo.findByPk(userId);
+      if (!user) {
+        await this.botRepo.create({
+          user_id: userId,
+          username: ctx.from.username,
+          first_name: ctx.from.first_name,
+          last_name: ctx.from.last_name,
+        });
+      }
+    } catch (error) {
+      console.error('Error in starting bot:', error);
+    }
+  }
 
+  async startCard(carData: any) {
+    try {
+      console.log('carData name:', carData.name);
+      console.log('carData id', carData.id);
+      console.log('carData yuvish', carData.yuvish);
+      console.log('carData name2', carData.name2);
 
+      const user = await this.carRepo.findByPk(carData.user_id);
 
-  
+      if (!user) {
+        await this.carRepo.create({
+          car_id: carData.id,
+          car_name: carData.name,
+          yuvish: carData.yuvish,
+          car_name2: carData.name2,
+        });
+      }
+    } catch (error) {
+      console.error('Error in starting bot:', error);
+    }
+  }
 }
